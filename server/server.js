290c117f74b9d3400876b05be8761a0971f539e4
Anas <anascodeworks@gmail.com>
2025-10-03 16:55:57 +0530

    :taada: feat(deploy): added cleint build to server

--- server/server.js
+++ server/server.js
@@ -1,30 +1,23 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import imageRouter from './routes/imageRoutes.js';
-import { connectDb } from './config/mongodb.js';
-import userRouter from './routes/userRoutes.js';
-
-
-
-
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import imageRouter from "./routes/imageRoutes.js";
+import { connectDb } from "./config/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(express.static("./static"));
 app.use(cors());
 await connectDb();
 
+app.use("/api/user", userRouter);
+app.use("/api/image", imageRouter);
+// app.get("/", (req, res) => {
+//   return res.send("This is home route");
+// });
 
-app.use('/api/user', userRouter);
-app.use('/api/image', imageRouter);
-app.get( '/' , (req, res)=>{
-  return   res.send(
-    "This is home route" )
-} )
-
-
-
-app.listen( PORT, ()=>{
-    console.log(`Server is listining on port ${PORT}`);
-} )
+app.listen(PORT, () => {
+  console.log(`Server is listining on port ${PORT}`);
+});
